d0c82dcca50a94320cfd00308c73b05fcd47c060
Taylor Gregory <tgdev91@gmail.com>
2020-08-16 13:52:53 -0400

    Implement WarCounterSubscriber to subscribe to warcounterticks

---
+++ Durable/WarCounterSubscriber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Durable.Entity;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using PointsBot.Infrastructure.Models;
+
+namespace Durable
+{
+    public class WarCounterSubscriber
+    {
+        private const string TopicName = "extensions";
+        private const string SubscriptionName = "WarCounter";
+
+        [FunctionName("WarCounterSubscriber")]
+        public Task TickWarCounter([DurableClient] IDurableEntityClient client,
+            [ServiceBusTrigger(TopicName, SubscriptionName, Connection = "ExtensionsConnectionString")]string message)
+        {
+            var counterTick = JsonSerializer.Deserialize<WarCounterTick>(message);
+            if (!WarId.TryParse($"{counterTick.SourceUser}_{counterTick.TargetUser}", out var warId))
+                throw new InvalidDataException($"Invalid warId: {warId}");
+
+            return client.SignalEntityAsync<ICounter>(new EntityId(nameof(WarCounter), warId),
+                counter => counter.Tick(counterTick.AmountTaken));
+        }
+    }
+}
